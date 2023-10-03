e5fe27c8241089532d3de71d88d9c1a920a8bb0b
Perera Dev <nashi.codes@gmail.com>
2023-10-03 02:47:24 -0300

    implementa fecho Transitivo Direto e indireto

--- Trabalho/src/Grafo.cpp
+++ Trabalho/src/Grafo.cpp
@@ -1,10 +1,6 @@
-//
-// Created by Nashi on 29/09/2023.
-//
-
 #include "Grafo.h"
 
-void Grafo::inserirNo(int idNo) {
+void Grafo::InserirNo(int idNo) {
     if (this->NOS.find(idNo) != this->NOS.end()) {
         cout << "Não foi possível criar o nó, pois ele já existe!" << endl;
         cout << "Deseja visualizar o nó? (s/n)" << endl;
@@ -15,11 +11,11 @@ void Grafo::inserirNo(int idNo) {
             no->printarNo(this->isPonderado(), this->isDirecionado());
         }
     } else {
-        this->criaNo(idNo);
+        this->CriaNo(idNo);
     }
 }
 
-void Grafo::criaNo(int idNo) {
+void Grafo::CriaNo(int idNo) {
     if (this->NOS.find(idNo) != this->NOS.end()) return;
     try {
         auto *no = new No(idNo);
@@ -30,17 +26,25 @@ void Grafo::criaNo(int idNo) {
     }
 }
 
-void Grafo::criarAresta(int idNoOrigem, int idNoDestino) {
+void Grafo::CriarAresta(int idNoOrigem, int idNoDestino) {
     if (this->NOS.find(idNoOrigem) == this->NOS.end() || this->NOS.find(idNoDestino) == this->NOS.end()) {
         throw invalid_argument("No nao existe!");
     } else {
         try {
-            auto *aresta = new Aresta(this->NOS[idNoDestino]);
-            this->NOS[idNoOrigem]->setAresta(idNoDestino, aresta);
+            auto *NoOrigem = this->NOS[idNoOrigem];
+            auto *NoDestino = this->NOS[idNoDestino];
+            auto *aresta = new Aresta(this->NOS[idNoOrigem], this->NOS[idNoDestino], ++this->NumArestas);
+
+            NoOrigem->setAresta(idNoDestino, aresta);
+            NoOrigem->setGrauSaida(NoOrigem->getGrauSaida() + 1);
+            NoDestino->setGrauEntrada(NoDestino->getGrauEntrada() + 1);
+
             if (!isDirecionado()) {
-                auto *aresta2 = new Aresta(this->NOS[idNoOrigem]);
-                this->NOS[idNoDestino]->setAresta(idNoOrigem, aresta2);
+                NoDestino->setAresta(idNoOrigem, aresta);
+                NoDestino->setGrauSaida(NoDestino->getGrauSaida() + 1);
+                NoOrigem->setGrauEntrada(NoOrigem->getGrauEntrada() + 1);
             }
+            this->ARESTAS[aresta->getID()] = aresta;
         } catch (exception &e) {
             cout << "Erro ao criar aresta!" << endl;
             throw e;
@@ -48,18 +52,24 @@ void Grafo::criarAresta(int idNoOrigem, int idNoDestino) {
     }
 }
 
-void Grafo::criarAresta(int idNoOrigem, int idNoDestino, int peso) {
+void Grafo::CriarAresta(int idNoOrigem, int idNoDestino, int peso) {
     if (this->NOS.find(idNoOrigem) == this->NOS.end() || this->NOS.find(idNoDestino) == this->NOS.end()) {
         throw invalid_argument("No nao existe!");
     } else {
         try {
-            auto *aresta = new Aresta(this->NOS[idNoDestino], peso);
-            this->NOS[idNoOrigem]->setAresta(idNoDestino, aresta);
+            auto *NoOrigem = this->NOS[idNoOrigem];
+            auto *NoDestino = this->NOS[idNoDestino];
+            auto *aresta = new Aresta(this->NOS[idNoOrigem], this->NOS[idNoDestino], this->NumArestas + 1, peso);
+            NoOrigem->setAresta(idNoDestino, aresta);
+            NoOrigem->setGrauSaida(NoOrigem->getGrauSaida() + 1);
+            NoDestino->setGrauEntrada(NoDestino->getGrauEntrada() + 1);
+            this->NumArestas++;
             if (!isDirecionado()) {
-                auto *aresta2 = new Aresta(this->NOS[idNoOrigem], peso);
-                this->NOS[idNoDestino]->setAresta(idNoOrigem, aresta2);
+                NoDestino->setAresta(idNoOrigem, aresta);
+                NoDestino->setGrauSaida(NoDestino->getGrauSaida() + 1);
+                NoOrigem->setGrauEntrada(NoOrigem->getGrauEntrada() + 1);
             }
-
+            this->ARESTAS[aresta->getID()] = aresta;
         } catch (exception &e) {
             cout << "Erro ao criar aresta!" << endl;
             throw e;
@@ -67,31 +77,97 @@ void Grafo::criarAresta(int idNoOrigem, int idNoDestino, int peso) {
     }
 }
 
-void Grafo::printArestas() {
-    cout << "Arestas: " << endl;
-    for (auto &no: this->NOS) {
-        for (auto &aresta: no.second->getArestas()) {
-            if (isDirecionado())
-                cout << no.first << " -> " << aresta.first << endl;
-            else {
-                No *noDestino = aresta.second->getNoDestino();
-                if (!noDestino->isVisitado())
-                    cout << no.first << " <-> " << aresta.first << endl;
-            }
-        }
-    }
-}
 
-void Grafo::printListaAdjacencia() {
+void Grafo::PrintListaAdjacencia() {
     cout << "Lista de Adjacência: " << endl;
     for (auto &no: this->NOS) {
         cout << "| " << no.first << " | : ";
         for (auto &aresta: no.second->getArestas()) {
-            if (!isPonderado())
-                cout << "-> |" << aresta.first << "| ";
+            if (this->isPonderado())
+                cout << "|" << aresta.first << "|(" << aresta.second->getPeso() << "), ";
             else
-                cout << "|" << aresta.first << "|(" << aresta.second->getPeso() << ")| -> ";
+                cout << "|" << aresta.first << "|, ";
+
+            cout << endl;
         }
         cout << endl;
     }
+}
+
+void Grafo::fechoTransitivoDireto(int idNo) {
+    if (!this->isDirecionado()) {
+        cout << "Grafo não direcionado!" << endl;
+        return;
+    }
+    if (this->NOS.find(idNo) == this->NOS.end()) {
+        cout << "Nó não existe!" << endl;
+        return;
+    }
+    auto *no = this->NOS[idNo];
+    cout << "Fecho Transitivo Direto: " << endl;
+    cout << "Nó: " << no->getID() << endl;
+    auto *nosVisitados = new set<No *>();
+    auxFTD(no, 0, nosVisitados);
+
+}
+
+void Grafo::auxFTD(No *no, int level, set<No *> *nosVisitados) {
+    if (no == nullptr) return;
+    if (nosVisitados->find(no) != nosVisitados->end()) return;
+    nosVisitados->insert(no);
+
+    for (auto &aresta: no->getArestas()) {
+        auto arestaDestino = aresta.second;
+        auto noDestino = arestaDestino->getNoDestino();
+        for (int i = 0; i < level; i++) {
+            cout << "---";
+        }
+        cout << ">" << noDestino->getID() << endl;
+        nosVisitados->insert(noDestino);
+        auxFTD(noDestino, level + 1, nosVisitados);
+    }
+}
+
+void Grafo::fechoTransitivoIndireto(int idNo) {
+    if (!this->isDirecionado()) {
+        cout << "Grafo não direcionado!" << endl;
+        return;
+    }
+    if (this->NOS.find(idNo) == this->NOS.end()) {
+        cout << "Nó não existe!" << endl;
+        return;
+    }
+    auto *no = this->NOS[idNo];
+    cout << "Fecho Transitivo Indireto: " << endl;
+    cout << "Nó: " << no->getID() << endl;
+    auto *nosVisitados = new set<No *>();
+    set<No *> *nosIncidentes = incidentes(no);
+    auxFTI(no, 0, nosVisitados, nosIncidentes);
+
+}
+
+void Grafo::auxFTI(No *no, int level, set<No *> *nosVisitados, set<No *> *nosIncidentes) {
+    if (no == nullptr) return;
+    if (nosVisitados->find(no) != nosVisitados->end()) return;
+    nosVisitados->insert(no);
+
+    for (auto &noIncidente: *nosIncidentes) {
+        for (int i = 0; i < level; i++) {
+            cout << "---";
+        }
+        cout << ">" << noIncidente->getID() << endl;
+        nosVisitados->insert(noIncidente);
+        set<No *> *auxNosIncidentes = incidentes(noIncidente);
+        auxFTI(noIncidente, level + 1, nosVisitados, auxNosIncidentes);
+    }
+}
+
+set<No *> *Grafo::incidentes(No *no) {
+    auto *nosIncidentes = new set<No *>();
+    for(auto &arestas : this->ARESTAS){
+        if(arestas.second->getNoDestino() == no){
+            nosIncidentes->insert(arestas.second->getNoOrigem());
+        }
+    }
+    return nosIncidentes;
 }
--- Trabalho/src/Grafo.h
+++ Trabalho/src/Grafo.h
@@ -19,17 +19,43 @@ public:
 
     Grafo(int ordem, bool ehPonderado, bool ehDirecionado) : AGrafo(ordem, ehPonderado, ehDirecionado) {};
 
-    void criaNo(int idNo);
+    void CriaNo(int idNo);
 
-    void inserirNo(int idNo);
+    void InserirNo(int idNo);
 
-    void criarAresta(int idNoOrigem, int idNoDestino);
+    void CriarAresta(int idNoOrigem, int idNoDestino);
 
-    void criarAresta(int idNoOrigem, int idNoDestino, int peso);
+    void CriarAresta(int idNoOrigem, int idNoDestino, int peso);
 
-    void printArestas();
+    void PrintListaAdjacencia();
 
-    void printListaAdjacencia();
+    void fechoTransitivoDireto(int idNo);
+
+    void fechoTransitivoIndireto(int idNo);
+
+    void Dijkstra(int idNoOrigem, int idNoDestino);
+
+    void Floyd(int idNoOrigem, int idNoDestino);
+
+    void AGMPrim(int idNoOrigem);
+
+    void AGMKruskal();
+
+    void DeepSearch(int idNo);
+
+    void ordenacaoTopologica();
+
+    void parametrosGrafo();
+
+    void articulacao();
+
+private:
+    void auxFTD(No *no, int level, set<No *> *visitados);
+
+    void auxFTI(No *no, int level, set<No *> *nosVisitados, set<No *> *nosIncidentes);
+
+    set<No *> *incidentes(No *no);
+
 };
 
 
